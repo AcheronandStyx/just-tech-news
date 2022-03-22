091853a58bbebd6f0291dced055ce16e68d38c9d
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-21 21:55:00 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,17 @@
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+module.exports = sequelize;
+
+/* Pre add of JawsDB
 const Sequelize = require('sequelize'); // import the Sequelize constructor from the library
 
 require('dotenv').config(); // import the protected credentials in the .env file from npm dotenv
@@ -9,4 +23,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   port: 3306
 });
 
-module.exports = sequelize;
+*/
