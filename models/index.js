0ba61415dcddbf955417e98935c1fd141c58e533
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-21 20:37:38 -0500

    13.3 done

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, { // links the users user_id to the user_id in the Post model
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, { // forges the linke in reverse
+    foreignKey: 'user_id',
+  });
+
 module.exports = { User, Post };
